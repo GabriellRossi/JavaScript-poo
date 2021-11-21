a5ef7c832e4dec980a425c08ff44f1b73ab914ef
Gabriel Rossi <gabriel.rossi159@gmail.com>
2021-11-21 04:02:02 -0300

    Melhorando a legibilidade utilizando o return

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ class contaCorrente {
   sacar(valor) {
       if(this._saldo >= valor) {
           this._saldo -= valor;
+          return valor;
       }
   }
   depositar(valor) {
-    if(valor > 0) {
-      this._saldo += valor;
+    if(valor <= 0) {
+      return;
     }
+    this._saldo += valor;
   }
 }
 
@@ -36,7 +38,7 @@ contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.sacar(50);
+const valorSacado = contaCorrenteRicardo.sacar(50);
 
-
+console.log(valorSacado);
 console.log(contaCorrenteRicardo);
